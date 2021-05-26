f0a44a2c9b279f2ad97386389aaab30c9e331977
Rodrigo Roma <rmarsan@estudio-marsan.com.ar>
2021-05-26 11:10:56 -0300

    agrego dashboard

--- routers/mainRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const mainController = require("../controllers/mainController");
-
-router.get("/", mainController.index);
-router.get("/home/:id", mainController.indexUser);
-router.get("/search", mainController.search);
-
-module.exports = router;
