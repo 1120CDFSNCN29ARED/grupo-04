74cc0722e6dfba09914541da9374a74f9427f489
polaquim83 <martingol@gmail.com>
2021-06-06 15:31:39 -0300

    vistas detalle proucto y venta

--- dashboard/src/components/Pedido.jsx
+++ dashboard/src/components/Pedido.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
 function Pedido(props) {
-    
     return (
         <div>
-            <span><img className="imgPedido" src={props.producto.imagenes.length > 0 ? `http://localhost:4000/img/products/${props.producto.imagenes[0].imagen}` : "http://localhost:4000/img/products/default.jpg"} alt="imagen de producto" /></span>
+            <span><img style= {{width: 15+"%"}} className="imgPedido" src={props.producto.imagenes.length > 0 ? `http://localhost:4000/img/products/${props.producto.imagenes[0].imagen}` : "http://localhost:4000/img/products/default.jpg"} alt="imagen de producto" /></span>
             <p className="nombrePedido">{props.producto.nombre}</p>
-            <p className="marcaPedido">{props.producto.marca.marca}</p>
-            <p className="catPedido">{props.producto.tipo_producto}</p>
-            <p className="cantidadPedido">{props.precio}</p>
-            <p className="precioPedido">{props.precio}</p>
+            <p className="marcaPedido">{props.producto.marca.nombre}</p>
+            <p className="cantidadPedido">{props.cantidad}</p>
+            <p className="precioPedido">{props.precio_compra}</p>
         </div>
     )
 }
--- site/routers/apiRoutes/users.js
+++ site/routers/apiRoutes/users.js
@@ -10,14 +10,4 @@ router.get("/total/:page", apiUsers.findAll);
 router.get("/:id", apiUsers.findOne);
 router.get("/img/:imagen", apiUsers.image);
 
-router.get("/productos", apiProductos.findAll);
-router.get("/productos/cat", apiProductos.findCategorias);
-router.get("/productos/cat/:cat", apiProductos.findAllByCat);
-router.get("/productos/last", apiProductos.findLast);
-router.get("/productos/:id", apiProductos.findOne);
-router.get("/compras", apiCompras.findAll);
-router.get("/compras/last", apiCompras.findLast);
-router.get("/compras/:id", apiCompras.findOne);
-router.get("/pedidos", apiPedidos.findAll);
-
 module.exports = router;
