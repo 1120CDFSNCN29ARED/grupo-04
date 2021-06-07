20c335d4af60394ec6875acd8bfaa6c52923589e
Rodrigo Roma <rmarsan@estudio-marsan.com.ar>
2021-06-07 11:12:42 -0300

    arregladas vistas para mobile

--- site/database/config/config.js
+++ site/database/config/config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    development: {
-        username: "root",
-        password: process.env.SQL_PASSWORD,
-        database: "pc_components",
-        loggin: false,
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
-    test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
-    production: {
-        username: process.env.SQL_USER,
-        password: process.env.SQL_PASSWORD,
-        database: process.env.SQL_DATABASE,
-        host: process.env.SQL_HOST,
-        dialect: "mysql",
-    },
+  development: {
+    username: "root",
+    //password: process.env.SQL_PASSWORD,
+    password: null,
+    database: "pc_components",
+    loggin: false,
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    host: process.env.SQL_HOST,
+    dialect: "mysql",
+  },
 };
